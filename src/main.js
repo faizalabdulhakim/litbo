eaeec89bcc7a3f56fe8f374b6319f7a74029149a
faizalabdulhakim <faizalabdulhakim07@gmail.com>
2024-10-13 23:55:06 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+const app = createApp(App);
+app
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+      options: {
+        darkModeSelector: '.my-app-dark',
+      },
+    },
+  })
+  .mount('#app');
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './src/App.vue'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
